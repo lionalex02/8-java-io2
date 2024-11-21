79e5aeadf873177e49982930f7eb43263a81f630
Ulearn 2.1-2.14 Юнусов ПрИ-202 <lionalex02@ya.ru>
2024-11-22 04:01:56 +0500

    8.1-8-3 YUNUSOV PrI-202

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -1,23 +1,41 @@
 package com.example.task02;
 
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class Task02Main {
     public static void main(String[] args) throws IOException, InterruptedException {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
 
-        /*
+
         System.out.println(listFiles(Paths.get("task02/src/main/resources/")));
-        */
+
 
     }
 
     public static List<Path> listFiles(Path rootDir) throws IOException, InterruptedException {
-        // your implementation here
+        List<Path> paths = new ArrayList<>();
 
-        return null;
+        try (Stream<Path> pathStream = Files.list(rootDir)) {
+            pathStream.forEach(path -> {
+                if(Files.isDirectory(path)) {
+                    try {
+                        paths.addAll(listFiles(path));
+                    } catch (IOException | InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+                if(Files.isRegularFile(path))
+                    paths.add(path);
+            });
+        }
+
+    return paths;
     }
 }
